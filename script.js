da927855f71f058ab42d2b47005be313b843e4bb
wishba <wisnubayu34@gmail.com>
2022-09-11 00:28:13 +0700

    display daily

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ function appendData(data) {
   let cardContainer = document.getElementById("card__container");
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
-    console.log(element);
+    // console.log(element.timeframes.daily);
+    // console.log(element.timeframes.daily.current);
     let div = document.createElement("div");
     div.innerHTML = `
       <p>${element.title}</p>
+      <p>${element.timeframes.daily.current}hrs</p>
+      <p>Yesterday - ${element.timeframes.daily.previous}hrs</p>
     `;
     cardContainer.appendChild(div);
   }
