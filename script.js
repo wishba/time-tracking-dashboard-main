43ccd5a0d502bfe3754ba75c01529661e3552a9d
wishba <wisnubayu34@gmail.com>
2022-09-10 22:35:54 +0700

    js file

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => appendData(data))
+
+function appendData(data) {
+  let cardContainer = document.getElementById("card__container");
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    console.log(element);
+    let div = document.createElement("div");
+    div.innerHTML = `
+          <p>${element.title}</p>
+        `;
+    cardContainer.appendChild(div);
+  }
+}
