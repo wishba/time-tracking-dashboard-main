fcc65d65d7240f082a5438141217ec4f44eab9fd
wishba <wisnubayu34@gmail.com>
2022-09-11 08:56:08 +0700

    display

--- script.js
+++ script.js
@@ -8,33 +8,40 @@ function appendData(object) {
     let cardContainer = document.getElementById("card__container");
     let div = document.createElement("div");
 
-    console.log(iterator.timeframes);
+    console.log(iterator);
 
     div.innerHTML = `
-      <p>${iterator.timeframes.daily.current}</p>
+      <p>${iterator.title}</p>
+      <p>${iterator.timeframes.daily.current}hrs</p>
+      <p>Yesterday - ${iterator.timeframes.daily.previous}hrs</p>
     `;
 
     document.getElementById("showDaily").addEventListener("click", daily);
     function daily() {
       div.innerHTML = `
-        <p>${iterator.timeframes.daily.current}</p>
+        <p>${iterator.title}</p>
+        <p>${iterator.timeframes.daily.current}hrs</p>
+        <p>Yesterday - ${iterator.timeframes.daily.previous}hrs</p>
       `;
     }
 
     document.getElementById("showWeekly").addEventListener("click", weekly);
     function weekly() {
       div.innerHTML = `
-        <p>${iterator.timeframes.weekly.current}</p>
+        <p>${iterator.title}</p>
+        <p>${iterator.timeframes.weekly.current}hrs</p>
+        <p>Last Week - ${iterator.timeframes.weekly.previous}hrs</p>
       `;
     }
 
     document.getElementById("showMonthly").addEventListener("click", monthly);
     function monthly() {
       div.innerHTML = `
-        <p>${iterator.timeframes.monthly.current}</p>
+        <p>${iterator.title}</p>
+        <p>${iterator.timeframes.monthly.current}hrs</p>
+        <p>Last Month - ${iterator.timeframes.monthly.previous}hrs</p>
       `;
     }
-
 
     cardContainer.appendChild(div);
   }
