9928d5fdb6da076f392678b1b8d705c3c2008145
wishba <wisnubayu34@gmail.com>
2022-09-11 09:54:17 +0700

    button style when clicked

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ fetch("data.json")
   .then(res => res.json())
   .then(object => appendData(object))
 
+let normalColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-report");
+let activeColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-main");
+
+let dayButton = document.getElementById("showDaily");
+let weekButton = document.getElementById("showWeekly");
+let monthButton = document.getElementById("showMonthly");
+
 function appendData(object) {
 
   for (const iterator of object) {
     let cardContainer = document.getElementById("report__card");
     let div = document.createElement("div");
 
-    console.log(iterator);
+    // console.log(iterator);
 
     div.innerHTML = `
       <p>${iterator.title}</p>
@@ -18,6 +25,10 @@ function appendData(object) {
 
     document.getElementById("showDaily").addEventListener("click", daily);
     function daily() {
+      dayButton.style.color = activeColor;
+      weekButton.style.color = normalColor;
+      monthButton.style.color = normalColor;
+
       div.innerHTML = `
         <p>${iterator.title}</p>
         <p>${iterator.timeframes.daily.current}hrs</p>
@@ -27,6 +38,10 @@ function appendData(object) {
 
     document.getElementById("showWeekly").addEventListener("click", weekly);
     function weekly() {
+      dayButton.style.color = normalColor;
+      weekButton.style.color = activeColor;
+      monthButton.style.color = normalColor;
+
       div.innerHTML = `
         <p>${iterator.title}</p>
         <p>${iterator.timeframes.weekly.current}hrs</p>
@@ -36,6 +51,10 @@ function appendData(object) {
 
     document.getElementById("showMonthly").addEventListener("click", monthly);
     function monthly() {
+      dayButton.style.color = normalColor;
+      weekButton.style.color = normalColor;
+      monthButton.style.color = activeColor;
+
       div.innerHTML = `
         <p>${iterator.title}</p>
         <p>${iterator.timeframes.monthly.current}hrs</p>
