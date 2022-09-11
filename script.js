1709a18b668dcf6828c9a8dc41db7e36d1433985
wishba <wisnubayu34@gmail.com>
2022-09-11 08:46:36 +0700

    interactive button

--- script.js
+++ script.js
@@ -3,14 +3,39 @@ fetch("data.json")
   .then(object => appendData(object))
 
 function appendData(object) {
+
   for (const iterator of object) {
     let cardContainer = document.getElementById("card__container");
     let div = document.createElement("div");
+
+    console.log(iterator.timeframes);
+
     div.innerHTML = `
-      <p>${iterator.title}</p>
-      <p>${iterator.timeframes.daily.current}hrs</p>
-      <p>Yesterday - ${iterator.timeframes.daily.previous}hrs</p>
+      <p>${iterator.timeframes.daily.current}</p>
+    `;
+
+    document.getElementById("showDaily").addEventListener("click", daily);
+    function daily() {
+      div.innerHTML = `
+        <p>${iterator.timeframes.daily.current}</p>
       `;
+    }
+
+    document.getElementById("showWeekly").addEventListener("click", weekly);
+    function weekly() {
+      div.innerHTML = `
+        <p>${iterator.timeframes.weekly.current}</p>
+      `;
+    }
+
+    document.getElementById("showMonthly").addEventListener("click", monthly);
+    function monthly() {
+      div.innerHTML = `
+        <p>${iterator.timeframes.monthly.current}</p>
+      `;
+    }
+
+
     cardContainer.appendChild(div);
   }
 }
