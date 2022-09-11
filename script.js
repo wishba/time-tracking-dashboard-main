9a3475d46ff7a63d494d65ebbb83552fb5743d29
wishba <wisnubayu34@gmail.com>
2022-09-11 11:40:16 +0700

    comment out

--- script.js
+++ script.js
@@ -2,59 +2,30 @@ fetch("data.json")
   .then(res => res.json())
   .then(object => appendData(object))
 
-let normalColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-report");
-let activeColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-main");
+// let normalColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-report");
+// let activeColor = window.getComputedStyle(document.documentElement).getPropertyValue("--txt-main");
 
-let dayButton = document.getElementById("showDaily");
-let weekButton = document.getElementById("showWeekly");
-let monthButton = document.getElementById("showMonthly");
-
-function dayHover() {
-  dayButton.style.color = activeColor;
-}
-function dayHoverOut() {
-  dayButton.style.color = normalColor;
-}
-
-function weekHover() {
-  weekButton.style.color = activeColor;
-}
-function weekHoverOut() {
-  weekButton.style.color = normalColor;
-}
-
-function monthHover() {
-  monthButton.style.color = activeColor;
-}
-function monthHoverOut() {
-  monthButton.style.color = normalColor;
-}
+// let dayButton = document.getElementById("showDaily");
+// let weekButton = document.getElementById("showWeekly");
+// let monthButton = document.getElementById("showMonthly");
 
 function appendData(object) {
-
   for (const iterator of object) {
     let cardContainer = document.getElementById("report__card");
     let div = document.createElement("div");
-    div.className = `report-${iterator.title}`;
-
-    // console.log(iterator);
 
     div.innerHTML = `
-      <div>
+      <div id="reportWeek">
         <p>${iterator.title}</p>
         <p>${iterator.timeframes.weekly.current}hrs</p>
-        <p>Yesterday - ${iterator.timeframes.weekly.previous}hrs</p>
+        <p>Last Week - ${iterator.timeframes.weekly.previous}hrs</p>
       </div>
     `;
 
     document.getElementById("showDaily").addEventListener("click", daily);
     function daily() {
-      dayButton.style.color = activeColor;
-      weekButton.style.color = normalColor;
-      monthButton.style.color = normalColor;
-
       div.innerHTML = `
-        <div>
+        <div id="reportDay">
           <p>${iterator.title}</p>
           <p>${iterator.timeframes.daily.current}hrs</p>
           <p>Yesterday - ${iterator.timeframes.daily.previous}hrs</p>
@@ -64,12 +35,8 @@ function appendData(object) {
 
     document.getElementById("showWeekly").addEventListener("click", weekly);
     function weekly() {
-      dayButton.style.color = normalColor;
-      weekButton.style.color = activeColor;
-      monthButton.style.color = normalColor;
-
       div.innerHTML = `
-        <div>
+        <div id="reportWeek">
           <p>${iterator.title}</p>
           <p>${iterator.timeframes.weekly.current}hrs</p>
           <p>Last Week - ${iterator.timeframes.weekly.previous}hrs</p>
@@ -79,12 +46,8 @@ function appendData(object) {
 
     document.getElementById("showMonthly").addEventListener("click", monthly);
     function monthly() {
-      dayButton.style.color = normalColor;
-      weekButton.style.color = normalColor;
-      monthButton.style.color = activeColor;
-
       div.innerHTML = `
-        <div>
+        <div id="reportMonth">
           <p>${iterator.title}</p>
           <p>${iterator.timeframes.monthly.current}hrs</p>
           <p>Last Month - ${iterator.timeframes.monthly.previous}hrs</p>
